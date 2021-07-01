911e4b9b83745e52401a868462211e6f176d99c1
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-01 14:44:06 -0400

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-
+    #SETTER for dog name
+    def name=(name)
+        @name = name
+    end
+    #GETTER for dog name
+    def name
+        @name
+    end
+    #SETTER for dog breed
+    def breed=(breed)
+        @breed = breed
+    end
+    #GETTER for dog breed
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
-
+    #SETTER for name
+    def name=(name)
+        @name = name
+    end
+    #GETTER for name
+    def name
+        @name
+    end
+    #SETTER for job
+    def job=(job)
+        @job = job
+    end
+    #GETTER for job
+    def job
+        @job
+    end
 end
